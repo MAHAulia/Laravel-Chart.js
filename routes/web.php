3df558cf0fc7f799e64b21c01d2ca2536bfe6a70
Moh. Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2020-05-01 12:06:47 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/data-controller', function () {
+    return view('welcome');
+});
+
+Route::get('/data-javascript', function () {
+    return view('welcome');
+});
